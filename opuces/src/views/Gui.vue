e33c8e9f2270fb55dbe46a502b32175401c15be2
Stphn <s.woirin.dev@gmail.com>
2022-01-20 17:04:33 +0100

    fin picto GUI

--- opuces/src/views/Gui.vue
+++ opuces/src/views/Gui.vue
@@ -4,19 +4,16 @@
     <PictoEye/>
     <PictoFb/>
     <PictoFilters/>
-    <PictoGoogle/>
-    <PictoHome/>
+    <PictoGoogle/>   
     <PictoInfo/>
     <PictoLocation/>
     <PictoMail/>
     <PictoNav/>
-    <PictoNav2/>
     <PictoNext/>
     <PictoRemove/>
     <PictoSearch/>
     <PictoStar/>
     <PictoStarEmpty/>
-    <PictoText/>
     <PictoUser/>
     <PictoDisplay1/>
     <PictoDisplay2/>
@@ -30,18 +27,15 @@ import PictoEye from '../components/PictoEye.vue'
 import PictoFb from '../components/PictoFb.vue'
 import PictoFilters from '../components/PictoFilters.vue'
 import PictoGoogle from '../components/PictoGoogle.vue'
-import PictoHome from '../components/PictoHome.vue'
 import PictoInfo from '../components/PictoInfo.vue'
 import PictoLocation from '../components/PictoLocation.vue'
 import PictoMail from '../components/PictoMail.vue'
 import PictoNav from '../components/PictoNav.vue'
-import PictoNav2 from '../components/PictoNav2.vue'
 import PictoNext from '../components/PictoNext.vue'
 import PictoRemove from '../components/PictoRemove.vue'
 import PictoSearch from '../components/PictoSearch.vue'
 import PictoStar from '../components/PictoStar.vue'
 import PictoStarEmpty from '../components/PictoStarEmpty.vue'
-import PictoText from '../components/PictoText.vue'
 import PictoUser from '../components/PictoUser.vue'
 import PictoDisplay1 from '../components/PictoDisplay1.vue'
 import PictoDisplay2 from '../components/PictoDisplay2.vue'
@@ -56,18 +50,15 @@ export default {
     PictoFb,
     PictoFilters,
     PictoGoogle,
-    PictoHome,
     PictoInfo,
     PictoLocation,
     PictoMail,
     PictoNav,
-    PictoNav2,
     PictoNext,
     PictoRemove,
     PictoSearch,
     PictoStar,
     PictoStarEmpty,
-    PictoText,
     PictoUser,
     PictoDisplay1,
     PictoDisplay2,
