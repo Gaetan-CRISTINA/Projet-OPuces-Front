e079e1d124c691ec15e974a5aaed59c3e2aec9d8
loictran <lok.tran@gmail.com>
2022-01-20 13:17:24 +0100

    mise en place du router et creation de la view GUI

---
+++ opuces/src/components/Titre1.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>COMPOSANT TITRE 1</h1>
+</template>
+
+<script>
+export default {
+  name: 'Titre1',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ opuces/src/views/Gui.vue
@@ -0,0 +1,14 @@
+<template>
+  <Titre1/>
+</template>
+
+<script>
+import Titre1 from '../components/Titre1.vue'
+
+export default {
+  name: 'Gui',
+  components: {
+    Titre1
+  }
+}
+</script>
