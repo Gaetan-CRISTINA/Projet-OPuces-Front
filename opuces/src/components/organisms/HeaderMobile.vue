5a08676013923d8610acb78700a84f014e1e6d69
loictran <lok.tran@gmail.com>
2022-01-24 13:38:56 +0100

    début intégration

--- opuces/src/components/organisms/HeaderMobile.vue
+++ opuces/src/components/organisms/HeaderMobile.vue
@@ -1,25 +1,23 @@
 <template>
   <header>
-    <div class="container-mobile">
-        <div class="flex">
-          <PictoUser/>
-          <p id="se-connecter">Se connecter</p>
+    <div class="main-container">
+      <div id="left-header-mobile" class="flex">
+        <PictoUser/>
+        <p id="se-connecter">Se connecter</p>
       </div>
-      <div class="flex">
-          <PictoFilters/>
+      <div id="right-header-mobile" class="flex">
+        <PictoFilters/>
+      </div>
+      <div id="left-header-desktop">
+        <a href="#"><span><Logo/></span>O'puces</a>
+      </div>
+      <div id="center-header-desktop" class="flex">
+        <SearchBar/>
+      </div>
+      <div id="right-header-desktop" class="flex">
+        <PictoUser/>
       </div>
     </div>
-
-    <div class="container-desktop">
-        <div>
-          <Logo/>
-          <p id="logo-typo">O'puces</p>
-        </div>
-        <div class="flex">
-            <PictoFilters/>
-        </div>
-    </div>
-    
   </header>
 </template>
 
@@ -27,25 +25,33 @@
 import PictoUser from '../atoms/PictoUser'
 import PictoFilters from '../atoms/PictoFilters'
 import Logo from '../atoms/Logo'
+import SearchBar from '../molecules/SearchBar'
 
 export default {
   name: 'HeaderMobile',
   components: {
       PictoUser,
       PictoFilters,
-      Logo
+      Logo,
+      SearchBar,
   },
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+@import '../../assets/scss/main';
+#left-header-desktop,
+#center-header-desktop,
+#right-header-desktop {
+  display: none;
+}
 header {
-  padding: 1em 15px;
+  padding: 1em 0;
   width: 100%;
   background-color: #fff;
   display: flex;
   }
-.container-mobile {
+.main-container {
   width: 100%;
   display: flex;
   justify-content: space-between;
@@ -60,30 +66,39 @@ header {
 }
   
   @media screen and (min-width: 576px) {
-    .container-mobile {
-      width: 540px;
-      margin: 0 auto;
-    }
+    
   }
   @media screen and (min-width: 768px) {
-    .container-mobile {
-      width: 720px;
-    }
+    
   }
   @media screen and (min-width: 992px) {
-    .container-mobile {
-      width: 960px;
+    #left-header-mobile,
+    #right-header-mobile {
+      display: none;
     }
-  }
+    #left-header-desktop,
+    #center-header-desktop,
+    #right-header-desktop {
+      display: block;
+    }
+    #left-header-desktop a {
+      display: flex;
+      font-weight: 900;
+      font-size: 16px;
+      color: $main-green;
+      transition: all .3s;
+    }
+    #left-header-desktop a span {
+      margin-right: .3em;
+    }
+    
+    
+  }  
   @media screen and (min-width: 1200px) {
-    .container-mobile {
-      width: 1140px;
-    }
+   
   }
   @media screen and (min-width: 1400px) {
-    .container-mobile {
-      width: 1320px;
-    }
+    
   }
 </style>
 
