abac903aa6499a641eeaf0d05510c18bf25ed592
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-01-26 13:49:48 +0100

    userprofil ok

---
+++ opuces/src/views/UserProfil.vue
@@ -0,0 +1,14 @@
+<template>
+  <FiltersMobile/>
+
+</template>
+
+<script>
+import FiltersMobile from '../components/organisms/FiltersMobile.vue'
+export default {
+  name: 'UserProfil',
+  components: {
+    FiltersMobile,
+  }
+}
+</script>
