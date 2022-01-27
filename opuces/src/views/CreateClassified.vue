6e7e7a3b608428e54e107c3b61aead44ef8959cc
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-01-27 21:17:45 +0100

    create-classified Mobile

--- opuces/src/views/CreateClassified.vue
+++ opuces/src/views/CreateClassified.vue
@@ -1,18 +1,108 @@
 <template>
-    <div>CreateClassifiedForm</div>
+  <div class="main-container">
+    <div class="logoOpuces">
+      <router-link :to="{ name: 'Home' }">
+        <span id="logo-link"><Logo /></span>
+        <h1>O'puces</h1>
+      </router-link>
+    </div>
+    <h1>CreateClassified Form</h1>
+    <form action="">
+      <h6>Titre de votre annonce</h6>
+      <label>
+        <input v-model="title" name="title" class="title" />
+      </label>
+
+      <h6>Description</h6>
+      <label>
+        <input v-model="description" name="description" class="description" />
+      </label>
+
+      <h6>Etat du produit</h6>
+      <label>
+        <select name="" id="">
+            <option value="">Etat du produit</option>
+        </select>
+      </label>
+
+      <h6>Catégorie</h6>
+      <label>
+        <select name="" id="">
+            <option value="">Catégorie</option>
+        </select>
+      </label>
+
+    <h6>Mode de livraions</h6>
+      <label>
+        <select name="" id="">
+            <option value="">Mode de livraison</option>
+        </select>
+      </label>
+    </form>
+
+    <h6>Images</h6>
+      <label>
+        <button>Upload Image</button>
+      </label>
+
+    <div class="homeLink">
+      <router-link
+        :to="{
+          name: 'Home',
+        }"
+      >
+        <button>Retouner vers la page d'accueil</button>
+      </router-link>
+    </div>
+  </div>
 </template>
 <script>
-
+import Logo from "../components/atoms/Logo.vue";
 export default {
   name: "CreateClassified",
   components: {
-  
+    Logo,
   },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../assets/scss/main";
+.main-container {
+  position: relative;
+  top: 70px;
+}
+button {
+  font-size: 12px;
+  font-weight: 700;
+  color: $main-green;
+  padding: 0.5em 1em;
+  border-radius: 22px;
+  border: solid 1px $main-green;
+  background-color: #fff;
+  margin-top: 0.5em;
+  transition: all 0.3s;
+  cursor: pointer;
+}
+button:hover {
+  background-color: $main-green;
+  color: #fff;
+  border: solid 1px $main-green;
+}
+.main-container div:first-child {
+  position: relative;
+  color: $main-green;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.logoOpuces{
+  margin-bottom: 20px;
+}
+.homeLink{
+    margin-top: 20px;
+}
 
 @media screen and (min-width: 576px) {
 }
