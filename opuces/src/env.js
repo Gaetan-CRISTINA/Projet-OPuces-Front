427d75e703d759f19e8c9be29a041b2563b11cd4
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-01-31 16:47:36 +0100

    env.js update

--- opuces/src/env.js
+++ opuces/src/env.js
@@ -4,5 +4,9 @@ export default{
 opucesApi: 'http://localhost/OPuces/public/wp-json/opuces/v1',
 
 
-wpApi : 'http://localhost/OPuces/public/wp-json/wp/v2'
+wpApi : 'http://localhost/OPuces/public/wp-json/wp/v2',
+
+jwtBaseURI: 'http://localhost/OPuces/public/wp-json/jwt-auth/v1',
+
+baseURI: 'http://localhost/OPuces/public/wp-json/opuces/v1/',
 }
