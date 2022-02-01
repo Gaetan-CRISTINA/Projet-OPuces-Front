288c030f38f5643d480382606498323614728147
loictran <lok.tran@gmail.com>
2022-02-01 09:56:37 +0100

    tab bar mobile ok

--- opuces/src/components/molecules/TabBar.vue
+++ opuces/src/components/molecules/TabBar.vue
@@ -1,50 +1,59 @@
 <template>
-  <footer>
-      <a href="#"><PictoHome/></a>
-      <a href="#"><PictoSearch/></a>
-      <a href="#"><PictoAdd1/></a>
-      <div class="add">
-          <a href="#"><PictoAdd2/></a>
-      </div>
-      <a href="#"><PictoAnnonces/></a>
-      <a href="#"><PictoCoeur/></a>
-  </footer>
+  <nav>
+      <PictoHome/>
+      <PictoSearch/>
+      <PictoAnnonces/>
+      <PictoAdd1 class="button-add"/>
+     
+  </nav>
 </template>
 
 <script>
 import PictoHome from '../atoms/PictoHome'
-import PictoSearch from '../atoms/PictoSearch.vue'
-import PictoAnnonces from '../atoms/PictoAnnonces.vue'
-import PictoCoeur from '../atoms/PictoCoeur.vue'
-import PictoAdd1 from '../atoms/PictoAdd1.vue'
-import PictoAdd2 from '../atoms/PictoAdd2.vue'
+import PictoAdd1 from '../atoms/PictoAdd1'
+import PictoAnnonces from '../atoms/PictoAnnonces'
+import PictoSearch from '../atoms/PictoSearch'
 
 export default {
   name: 'TabBar',
   components: {
       PictoHome,
-      PictoSearch,
-      PictoAnnonces,
-      PictoCoeur,
       PictoAdd1,
-      PictoAdd2,
+      PictoAnnonces,
+      PictoSearch,
   }
 }
 </script>
 
+<style lang="scss">
+#picto-add1 {
+    position: relative;
+    width: 30px;
+    left: 22px;
+    top: 22px;
+}
+</style>
+
 <style scoped lang="scss">
 @import "../../assets/scss/main.scss";
 
-footer {
-    height:63px;;
-    width:375px;
-    background-color:$light-grey;
+nav {
+    position: fixed;
+    z-index: 999;
+    bottom: 0;
+    width:100%;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
 }
-
-.add {
-    display:none;
+.button-add {
+    position: relative;
+    bottom: 35px;
+    width: 75px;
+    height: 75px;
+    border-radius: 100%;
+    -webkit-box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.16);
+    -moz-box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.16);
+    box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.16);
 }
 </style>
