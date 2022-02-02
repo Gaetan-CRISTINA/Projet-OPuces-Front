90aab31594036f5e43559c3282aaf20b1571d5c9
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-02-02 17:49:21 +0100

    hot fix-gaetan

--- opuces/src/views/Search.vue
+++ opuces/src/views/Search.vue
@@ -1,14 +1,38 @@
 <template>
-  <Header/>
+  <div>
+    <Header />
+    <div class="main-container">
+      <FilterDesktop />
+    </div>
+  </div>
 </template>
 
 <script>
-import Header from '../components/organisms/Header.vue'
-
+import Header from "../components/organisms/Header.vue";
+import FilterDesktop from "../components/organisms/FiltersDesktop.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Header,
-  }
-}
+    FilterDesktop,
+  },
+};
 </script>
+
+<style scoped lang="scss">
+@import "../assets/scss/main";
+
+@media screen and (min-width: 576px) {
+}
+@media screen and (min-width: 768px) {
+  
+}
+@media screen and (min-width: 992px) {
+  
+}
+@media screen and (min-width: 1200px) {
+}
+@media screen and (min-width: 1400px) {
+}
+
+</style>
