b7f908f9ad097ddaa3dea0dc039f594b5a15828f
Etudiant O'clock <gaetan.cristina@gmail.com>
2022-02-03 11:18:51 +0100

    about ok

--- opuces/src/views/About.vue
+++ opuces/src/views/About.vue
@@ -2,8 +2,9 @@
   <div class="about">
     <Header />
     <div class="main-container">
+      <h2>L'Equipe</h2>
       <div class="picDev">
-        <h2>L'Equipe</h2>
+        
         <span>
           <img src="../assets/pictures/Mcdo.png" alt="" />
           <h3>Stephen Woirin</h3>
@@ -67,8 +68,9 @@ export default {
 
 <style scoped lang="scss">
 @import "../assets/scss/main";
-@media screen and (max-width: 576px) {
-  .picDev img {
+
+
+.picDev img {
     width: 200px;
     border-radius: 100%;
     margin-bottom: 20px;
@@ -83,7 +85,8 @@ export default {
   }
   .picDev span {
     border: 2px solid #31c7c0;
-    width: 210px;
+    height: 450px;
+    width: 300px;
     border-radius: 20px;
     background-color: #f5f5ee;
     margin-bottom: 20px;
@@ -99,15 +102,96 @@ export default {
     text-align: center;
   }
   h2{
+    padding-top: 5rem;
+  }
+  .role, .link, h3{
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+
+@media screen and (max-width: 576px) {
+  .picDev{
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding-top: 0;
+    
+  }
+  h2{
+    padding: 6rem 0 0 0;
+   
+  }
+  .picDev span{
+    height: 450px;
+    width: 300px;
+    margin: 0 20px 0 20px;
     margin-bottom: 20px;
   }
 }
 @media screen and (min-width: 768px) {
+  .picDev{
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding-top: 0;
+    
+  }
+  h2{
+    padding: 6rem;
+    
+    
+  }
+  .picDev span{
+    height: 450px;
+    width: 300px;
+    margin: 0 20px 0 20px;
+    margin-bottom: 20px;
+  }
 }
 @media screen and (min-width: 992px) {
+  .picDev{
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding-top: 0;
+  }
+  h2{
+    padding: 6rem;
+    
+  }
+  .picDev span{
+    height: 450px;
+    width: 300px;
+    margin: 0 20px 0 20px;
+    margin-bottom: 20px;
+  }
 }
 @media screen and (min-width: 1200px) {
+  .picDev{
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding-top: 0;
+  }
+  h2{
+    padding: 6rem;
+  }
+  .picDev span{
+    height: 450px;
+    width: 300px;
+    margin: 0 20px 0 20px;
+    margin-bottom: 20px;
+  }
 }
 @media screen and (min-width: 1400px) {
+  .picDev{
+    flex-direction: row;
+    padding-top: 0;
+    
+  }
+  h2{
+    padding: 6rem;
+  }
+  .picDev span{
+    height: 450px;
+    width: 300px;
+    margin: 0 20px 0 20px;
+  }
 }
 </style>
