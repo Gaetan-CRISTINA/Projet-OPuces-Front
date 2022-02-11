1d6ee0c705a13ed834b15551506f2ddfb9c69b86
Stphn <s.woirin.dev@gmail.com>
2022-02-11 10:20:33 +0100

    add update email+password

--- opuces/src/views/UpdatePassword.vue
+++ opuces/src/views/UpdatePassword.vue
@@ -50,7 +50,7 @@
             />
           </label>
           <div class="error" v-if="newPasswordVerifyEmpty">
-            Vous devez saisir un password
+            Vous devez saisir un mot de passe
           </div>
           <div class="error" v-if="newPasswordConfirm">
             Les mots de passe ne correspondent pas.
@@ -153,7 +153,11 @@ export default {
   },
 };
 </script>
-
+<style  lang="scss">
+@import "../assets/scss/main.scss";
+.error {padding:5px 0 5px 0; color:$social-google;
+}
+</style>
 <style scoped lang="scss">
 @import "../assets/scss/main.scss";
 .main-container {
@@ -167,12 +171,15 @@ export default {
 .left, .right{
   margin-top: 10rem;
 }
-.left{
+.left {
   display: flex;
+  
+  justify-content:center;
   flex-direction: column;
   
 }
-button {
+
+.--button {
   margin-top: 2em;
   background-color: $main-green;
   border-radius: 19px;
@@ -189,6 +196,7 @@ button {
 button:hover {
   background-color: $secondary-green;
 }
+
 input {
   height: 38px;
   background-color: $light-grey;
